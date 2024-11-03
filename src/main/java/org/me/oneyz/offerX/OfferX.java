e692854b5115a0fb42aece0ddcd17d68645108a3
Avzti <avzti@none.com>
2024-11-03 21:30:54 +0100

    Initialize

--- src/main/java/org/me/oneyz/offerX/OfferX.java
+++ src/main/java/org/me/oneyz/offerX/OfferX.java
@@ -7,7 +7,7 @@ public final class OfferX extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        System.out.println("Masteer");
+        System.out.println("Maseteer");
 
     }
 
