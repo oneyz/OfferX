7eec95ada725deb6e4d8f1f4e249bb057726bc50
Avzti <avzti@none.com>
2024-11-03 23:08:33 +0100

    Changes

--- src/main/java/org/me/oneyz/offerX/OfferX.java
+++ src/main/java/org/me/oneyz/offerX/OfferX.java
@@ -1,18 +1,18 @@
 package org.me.oneyz.offerX;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.me.oneyz.offerX.commands.Trade;
+
+import java.util.Objects;
 
 public final class OfferX extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-        System.out.println("Maseteer");
-
+        Objects.requireNonNull(getCommand("trade")).setExecutor(new Trade());
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
